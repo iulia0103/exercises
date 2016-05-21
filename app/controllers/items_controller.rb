3328605ed7ab1ed74fb7b8bfec68b5ba68a74da5
iulia0103 <iulia.leancu@gmail.com>
2016-05-21 15:06:25 +0200

    add actions and links for: new, create, destroy; add all products page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,4 +7,32 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
     @jewelry = Jewelry.find(params[:jewelry_id])
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+
+    if @item.save
+      redirect_to jewelry_item_path(@item.jewelry_id, @item.id)
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+
+    @item.destroy
+
+    redirect_to "/jewelries/:jewelry_id/items"
+  end
+
+  private
+
+  def item_params
+    item_params = params.require( :item ).permit( :name, :code, :price, :jewelry_id )
+  end
 end
